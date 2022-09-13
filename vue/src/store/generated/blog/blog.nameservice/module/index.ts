c0a50885483ea80f759e44e37ac3fb8db7eb2ae9
5iddy <siddhuyeturi@gmail.com>
2022-09-13 10:26:43 +0530

    ignite scaffold map whois name value price owner --no-message --module nameservice

--- vue/src/store/generated/blog/blog.nameservice/module/index.ts
+++ vue/src/store/generated/blog/blog.nameservice/module/index.ts
@@ -4,15 +4,15 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
-import { MsgDeleteName } from "./types/nameservice/tx";
-import { MsgBuyName } from "./types/nameservice/tx";
 import { MsgSetName } from "./types/nameservice/tx";
+import { MsgBuyName } from "./types/nameservice/tx";
+import { MsgDeleteName } from "./types/nameservice/tx";
 
 
 const types = [
-  ["/blog.nameservice.MsgDeleteName", MsgDeleteName],
-  ["/blog.nameservice.MsgBuyName", MsgBuyName],
   ["/blog.nameservice.MsgSetName", MsgSetName],
+  ["/blog.nameservice.MsgBuyName", MsgBuyName],
+  ["/blog.nameservice.MsgDeleteName", MsgDeleteName],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -45,9 +45,9 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
-    msgDeleteName: (data: MsgDeleteName): EncodeObject => ({ typeUrl: "/blog.nameservice.MsgDeleteName", value: MsgDeleteName.fromPartial( data ) }),
-    msgBuyName: (data: MsgBuyName): EncodeObject => ({ typeUrl: "/blog.nameservice.MsgBuyName", value: MsgBuyName.fromPartial( data ) }),
     msgSetName: (data: MsgSetName): EncodeObject => ({ typeUrl: "/blog.nameservice.MsgSetName", value: MsgSetName.fromPartial( data ) }),
+    msgBuyName: (data: MsgBuyName): EncodeObject => ({ typeUrl: "/blog.nameservice.MsgBuyName", value: MsgBuyName.fromPartial( data ) }),
+    msgDeleteName: (data: MsgDeleteName): EncodeObject => ({ typeUrl: "/blog.nameservice.MsgDeleteName", value: MsgDeleteName.fromPartial( data ) }),
     
   };
 };
