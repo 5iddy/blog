7e6ee96c8225892a1c22598781b1365b91cb59b1
5iddy <siddhuyeturi@gmail.com>
2022-09-13 10:20:57 +0530

    ignite scaffold message set-name name value --module nameservice

--- vue/src/store/generated/blog/blog.nameservice/module/index.ts
+++ vue/src/store/generated/blog/blog.nameservice/module/index.ts
@@ -5,10 +5,12 @@ import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
 import { MsgBuyName } from "./types/nameservice/tx";
+import { MsgSetName } from "./types/nameservice/tx";
 
 
 const types = [
   ["/blog.nameservice.MsgBuyName", MsgBuyName],
+  ["/blog.nameservice.MsgSetName", MsgSetName],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -42,6 +44,7 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
     msgBuyName: (data: MsgBuyName): EncodeObject => ({ typeUrl: "/blog.nameservice.MsgBuyName", value: MsgBuyName.fromPartial( data ) }),
+    msgSetName: (data: MsgSetName): EncodeObject => ({ typeUrl: "/blog.nameservice.MsgSetName", value: MsgSetName.fromPartial( data ) }),
     
   };
 };
